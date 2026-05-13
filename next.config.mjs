6c86f8b823ae66c86927730bd5bfde76ef2f5633
Pial <pialname394@gmail.com>
2026-05-13 16:58:16 +0600

    create a next config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  experimental: {
+    turbo: {
+      resolveAlias: {
+        "@": "./src",
+      },
+    },
+  },
 };
 
 export default nextConfig;
