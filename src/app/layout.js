77db472ae5c1db9a205384160a96f20e5e2910ba
Pial <pialname394@gmail.com>
2026-05-15 15:06:41 +0600

    added a google font from next-font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,6 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Josefin_Sans } from "next/font/google";
 import "./globals.css";
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const josefin = Josefin_Sans({
   subsets: ["latin"],
 });
 
@@ -17,10 +11,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
+    <html lang="en" className={`${josefin.className} h-full antialiased`}>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
